fff672daa4ea0c9e5d1ad14a043892a9356c026b
Rdennett3 <r.dennett3@gmail.com>
2024-05-09 11:01:54 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `May-2024-portfolio`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
