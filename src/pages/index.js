4e16012456ff61f8dd93aa6d8593593aac80c671
Robert Dennett <r.dennett3@gmail.com>
2025-12-10 13:32:33 -0500

    updated import for emailjs

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,11 @@ import HorizontalScroll from "../components/horizontal-work"
 import ContactForm from "../components/ContactForm"
 import WeatherWidget from "../components/WeatherWidget"
 // import ImgExpand from "../components/old-unused/img-expand"
+import emailjs from '@emailjs/browser';
+
+const SERVICE_ID = "service_v8hknre";
+const TEMPLATE_ID = "template_ccs4h2r";
+const PUBLIC_KEY = "WVzM9ljnTFMYxLvZb";
 
 const IndexPage = () => {
   return (
